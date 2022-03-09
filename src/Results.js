5f1b51ba97c5d7f8b73dee85595eae0406bc76c4
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2022-03-09 20:27:02 +0100

    Created function Meaning

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Meaning from "./Meaning";
 import "./Results.css";
 
 export default function Results(props) {
@@ -7,7 +8,11 @@ export default function Results(props) {
       <div className="Results">
         <h2>{props.results.word}</h2>
         {props.results.meanings.map(function (meaning, index) {
-          return meaning.definitions[0].definition;
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
         })}
       </div>
     );
