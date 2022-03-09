49ce96f4fe6777a660289b801c929f114d9d0554
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2022-03-09 20:20:31 +0100

    Created function results

--- src/Results.js
+++ src/Results.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./Results.css";
 
-export default function Result(props) {
-  if (props.results.length) {
-    return <div className="Results"></div>;
+export default function Results(props) {
+  if (props.results) {
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        {props.results.meanings.map(function (meaning, index) {
+          return meaning.definitions[0].definition;
+        })}
+      </div>
+    );
   } else {
     return null;
   }
