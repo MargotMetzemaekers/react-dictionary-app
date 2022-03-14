c8635215477436ce18310a0102c9505b03c7abc5
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2022-03-14 20:22:34 +0100

    Added audio and text

---
+++ src/Phonetic.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  return (
+    <div className="phonetic">
+      <a href={props.phonetic.audio} target="_blank">
+        {" "}
+        Listen 🔊{" "}
+      </a>
+      <br />
+      {props.phonetic.text}
+    </div>
+  );
+}
