6312bd0b3edfcdce883b63669b8bed5f436d64f3
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2022-03-19 13:36:56 +0100

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,7 +4,7 @@ import Results from "./Results";
 import Photos from "./Photos";
 import "./Dictionary.css";
 
-export default function Dictionary() {
+export default function Dictionary(props) {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
   let [photos, setPhotos] = useState(null);
