17688d2c8f88490ecb6aa5f10692cd00439e558f
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2022-03-19 13:18:46 +0100

    Added Photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
 
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000011e6ed10851c449bbb26db46136075803";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -29,12 +39,13 @@ export default function Dictionary(props) {
           <input
             type="search"
             onChange={handleKeywordChange}
-            placeholder="Tupe a word..."
+            placeholder="Type a word..."
           />
         </form>
-        <div classname="hint">Suggested keywords: sunset, dog, plant...</div>
+        <div className="hint">Suggested keywords: sunset, dog, plant...</div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
